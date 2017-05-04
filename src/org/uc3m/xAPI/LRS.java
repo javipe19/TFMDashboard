c77ad21e92f7347dccbb487c4fadb044ce21dbe1
Javier Peña <javipe19@gmail.com>
2017-05-04 22:14:20 +0200

    Adding methods to LRS class

--- src/org/uc3m/xAPI/LRS.java
+++ src/org/uc3m/xAPI/LRS.java
@@ -2,9 +2,13 @@ package org.uc3m.xAPI;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -12,23 +16,39 @@ import java.util.Set;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import gov.adlnet.xapi.client.ActivityClient;
+import gov.adlnet.xapi.client.AgentClient;
 import gov.adlnet.xapi.client.StatementClient;
+import gov.adlnet.xapi.model.Actor;
 import gov.adlnet.xapi.model.Statement;
 import gov.adlnet.xapi.model.StatementResult;
+import gov.adlnet.xapi.model.Verbs;
 
 public class LRS {
 	String LRS_URI = "http://62.204.199.105/data/xAPI/";
 	String USERNAME = "a0034dc9684deff87aea9f4354dd2b2925d92391";
 	String PASSWORD = "5d6e85d7072b6b769aa79121186e693ea813c84f";
 	ArrayList<String> activitiesIds = new ArrayList<String>();
+	StatementClient stmtClient;
+	AgentClient agentClient;
+	ActivityClient actClient;
 	
 	public LRS(){
+		try {
+			stmtClient = new StatementClient(LRS_URI, USERNAME,	PASSWORD);
+			agentClient = new AgentClient(LRS_URI, USERNAME,PASSWORD);
+			actClient = new ActivityClient(LRS_URI, USERNAME,PASSWORD);
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		activitiesIds.add("http://adlnet.gov/expapi/activities/assessment");
 		activitiesIds.add("http://adlnet.gov/expapi/activities/attempt");
 		activitiesIds.add("http://adlnet.gov/expapi/activities/course");
 		activitiesIds.add("http://adlnet.gov/expapi/activities/file");
 		activitiesIds.add("http://adlnet.gov/expapi/activities/interaction");
-		activitiesIds.add("http://adlnet.gov/expapi/activities/lesson");
+		//activitiesIds.add("http://adlnet.gov/expapi/activities/lesson");
 		activitiesIds.add("http://adlnet.gov/expapi/activities/link");
 		activitiesIds.add("http://adlnet.gov/expapi/activities/media");
 		activitiesIds.add("http://adlnet.gov/expapi/activities/meeting");
@@ -42,22 +62,20 @@ public class LRS {
 	}
 	
 	public String getActivities(){
-		HashMap<String, Integer> activities = new HashMap<String, Integer>();
 		String acts = null;
 		try {
-			StatementClient client = new StatementClient(LRS_URI, USERNAME,	PASSWORD);
+	
 			JSONArray jsonarray= new JSONArray();
 			for(int i=0;i<activitiesIds.size();i++){
-				StatementResult results = client.filterByActivity(activitiesIds.get(i)).getStatements();
+				StatementResult results = stmtClient.filterByActivity(activitiesIds.get(i)).getStatements();
 				ArrayList<Statement> statements = results.getStatements();
-				ArrayList<String> stmtsArray = statementsToArray(statements);
-				Set<String> unique = new HashSet<String>(stmtsArray);
+				ArrayList<String> activityArray = statementsToActivityArray(statements);
+				Set<String> unique = new HashSet<String>(activityArray);
 				for (String key : unique) {				
 				    //System.out.println(key + ": " + Collections.frequency(stmtsArray, key));
-				    activities.put(key, Collections.frequency(stmtsArray, key));
 					JSONObject json = new JSONObject();
 					json.put("name", key.toString());
-					json.put("frequency", Collections.frequency(stmtsArray, key));
+					json.put("frequency", Collections.frequency(activityArray, key));
 					jsonarray.add(json);
 				}
 				
@@ -71,23 +89,91 @@ public class LRS {
 			e.printStackTrace();
 		}
 		
-		
-		
 		return acts;
 		
 	}
 	
-	public ArrayList<String> statementsToArray(ArrayList<Statement> statements){
-		ArrayList<String> stmts = new ArrayList<String>();
+	public ArrayList<String> statementsToActivityArray(ArrayList<Statement> statements){
+		ArrayList<String> activities = new ArrayList<String>();
 		for(int j=0; j<statements.size();j++){
-			String stmt = statements.get(j).toString();
-			stmt = stmt.substring(stmt.indexOf(" ", stmt.indexOf(" ") + 1)+1, stmt.length()); //second occurrence of " "
-			stmts.add(stmt);
+			String activity = getStatementActivity(statements.get(j));
+			activities.add(activity);
 		}
 
-		return stmts;
+		return activities;	
+	}
+	
+	public String getStatementActivity(Statement statement){
+		String stmt = statement.toString();
+		stmt = stmt.substring(stmt.indexOf(" ", stmt.indexOf(" ") + 1)+1, stmt.length()); //second occurrence of " "
+		return stmt;
+	}
+	
+	public String getPagesAndTimes(String actor){
+		String s=null;
+		try {
+			JSONArray jsonarray= new JSONArray();
+			StatementResult results = stmtClient.filterByVerb(Verbs.terminated()).filterByActivity("http://adlnet.gov/expapi/activities/lesson").getStatements();
+			ArrayList<Statement> statements = results.getStatements();
+			for(int i=0; i<statements.size(); i++){
+				Statement stmt = statements.get(i);
+				if(stmt.toString().contains(actor)){
+					String page = getStatementActivity(stmt);
+					String duration = stmt.getResult().getDuration();
+					Duration d = Duration.parse(duration);
+					long seconds = d.getSeconds();
+					JSONObject json = new JSONObject();
+					json.put("page", page);
+					json.put("duration", seconds);
+					jsonarray.add(json);
+				}
+			}
+			
+			s = jsonarray.toJSONString();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return s;
 		
-		
+	}
+	
+	public String getDates(String actor){
+		String s=null;
+		ArrayList<String> temp = new ArrayList<String>();
+		try {
+			JSONArray jsonarray= new JSONArray();
+			StatementResult results = stmtClient.getStatements();
+			ArrayList<Statement> statements = results.getStatements();
+			for(int i=0; i<statements.size(); i++){
+				Statement stmt = statements.get(i);
+				if(stmt.toString().contains(actor)){
+					String timestamp = stmt.getTimestamp();
+					if (!temp.contains(timestamp)){
+						SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+						SimpleDateFormat output = new SimpleDateFormat("yyyy-MM-dd");
+						Date d = sdf.parse(timestamp.substring(0,18));
+						String formattedTime = output.format(d);
+						JSONObject json = new JSONObject();
+						json.put("date", formattedTime);
+						jsonarray.add(json);
+					}
+				}
+			}
+			
+			s = jsonarray.toJSONString();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return s;
 	}
 
 }
